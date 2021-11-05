18acd70e67b1a9cf7783797ec2734384b26484d7
Iñiguez Guizar Erandy Sirein <iniguez.guizar.erandy.sirein@gmail.com>
2021-11-05 10:05:18 -0600

    tercera version

---
+++ app/src/main/java/com/erandy/sirein/iniguez/holamundoandroid/logica/Calculadora.java
@@ -0,0 +1,27 @@
+package com.erandy.sirein.iniguez.holamundoandroid.logica;
+
+import java.io.Serializable;
+
+public class Calculadora implements Serializable
+{
+    public void suma()
+    {
+
+    }
+
+    public void resta()
+    {
+
+    }
+
+    public void multiplicacion()
+    {
+
+    }
+
+    public void division()
+    {
+
+    }
+
+}
